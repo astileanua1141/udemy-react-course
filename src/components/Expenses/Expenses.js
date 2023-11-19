3866deebf1e89c80b3088a3848555c915eb283e8
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-11-19 15:47:56 +0200

    Package restructuring and fix small typos

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props) {
@@ -9,22 +9,22 @@ function Expenses(props) {
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={props.items[1].title}
         amount={props.items[1].amount}
         date={props.items[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={props.items[2].title}
         amount={props.items[2].amount}
         date={props.items[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      ></ExpenseItem>
+      />
     </Card>
   );
 }
